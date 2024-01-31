aedc057f64fb97382d1d83555bad5e1f455cd297
Jake <jakem18@gmail.com>
2024-01-30 21:36:06 -0500

    first commit

---
+++ Tournament.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+import player
+import random
+#set some basic variables. reading the CSV file, getting how many players there are
+playersinput = pd.read_csv("players.csv", dtype={'playerID':'str'})
+playercount = len(playersinput)
+playerspergame = 2
+playerlist = []
+i=0
+
+#creates the players
+while i < playercount:
+    firstname = playersinput["FirstName"].loc[[i]]
+    lastname = playersinput["LastName"].loc[[i]]
+    id = playersinput["playerID"].loc[[i]]
+    new_player = player.Player(id, firstname, lastname)
+    playerlist.append(new_player)
+    #print(new_player.lastname)
+
+    i = i+1
+
+teir1games = playercount/playerspergame
+random.shuffle(playerlist)
+
+
+
+
+
---
+++ player.py
@@ -0,0 +1,8 @@
+class Player(object):
+    def __init__(self,id,firstname,lastname, wins = 0, lose = 0):
+        self.id = id
+        self.firstname = firstname
+        self.lastname = lastname
+        self.wins = 0
+        self.lose = 0
+
